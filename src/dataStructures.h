3369adee3d09b4ff88514d2a29a0178144d4141f
MM <manufacturevn@gmail.com>
2021-04-02 11:17:35 +0700

    first commit

---
+++ src/dataStructures.h
@@ -0,0 +1,21 @@
+#ifndef dataStructures_h
+#define dataStructures_h
+
+#include <vector>
+#include <opencv2/core.hpp>
+
+
+struct DataFrame { // represents the available sensor information at the same time instance
+    
+    cv::Mat cameraImg; // camera image
+    
+    std::vector<cv::KeyPoint> keypoints; // 2D keypoints within camera image
+    cv::Mat descriptors; // keypoint descriptors
+    std::vector<cv::DMatch> kptMatches; // keypoint matches between previous and current frame
+};
+
+struct LidarPoint { // single lidar point in space
+    double x,y,z,r; // x,y,z in [m], r is point reflectivity
+};
+
+#endif /* dataStructures_h */
